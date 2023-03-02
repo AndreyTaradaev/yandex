fc756b0b13dd376b69c59ea135120a4fad97e5a3
AndreyTaradaev <andrey@tvsi.ru>
2023-03-02 17:52:18 +0700

    change 32 zad

--- 32y/main.go
+++ 32y/main.go
@@ -3,19 +3,19 @@ package main
 /*
 
 
-Дан неориентированный невзвешенный граф. Необходимо посчитать количество его компонент связности и
+Дан неориентированный невзвешенный граф. Необходимо посчитать количество его компонент связности и 
 вывести их.
 
 Формат ввода
 
-Во входном файле записано два числа N и M (0 < N ≤ 100000, 0 ≤ M ≤ 100000). В следующих M строках
+Во входном файле записано два числа N и M (0 < N ≤ 100000, 0 ≤ M ≤ 100000). В следующих M строках 
 записаны по два числа i и j (1 ≤ i, j ≤ N), которые означают, что вершины i и j соединены ребром.
 
 Формат вывода
 
-В первой строчке выходного файла выведите количество компонент связности. Далее выведите сами компоненты
-связности в следующем формате: в первой строке количество вершин в компоненте, во второй - сами вершины в
-произвольном порядке. .
+В первой строчке выходного файла выведите количество компонент связности. Далее выведите сами компоненты 
+связности в следующем формате: в первой строке количество вершин в компоненте, во второй - сами вершины в 
+произвольном порядке.  
 */
 
 import (
@@ -25,20 +25,16 @@ import (
 	//	"sort"
 )
 
-type comp struct {
-	vector int
-	c      int
+func dfs (graf [][]int,visited  [] int,  v,group  int )  {
+	visited[v] = group
+	for i := 0; i < len(graf[v]) ; i++ {
+		if(visited[graf[v][i]] != group )	{
+			dfs(graf,visited, graf[v][i],group )
+		}		
+	}	
 }
 
-func dfs(graf [][]int,  v, comp int) {
-	visited[v] = true
-	for i := 0; i < len(graf[v]); i++ {
-		if visited[graf[v][i].vector] == false {
-			graf[v][i].c = comp
-			dfs(graf, visited, graf[v][i].vector, comp)
-		}
-	}
-}
+
 
 func main() {
 	//var command string
@@ -49,47 +45,70 @@ func main() {
 		return
 	}
 	defer file.Close()
-	rd := bufio.NewReader(file)    // create reader
-	_, err = fmt.Fscan(rd, &N, &M) //   command
+	rd := bufio.NewReader(file) // create reader
+	_, err = fmt.Fscan(rd, &N,&M)  //   command
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	ns := make([][]comp, N+1, N+1)
+	ns := make([] []int, N+1, N+1)
 	for i := 0; i < len(ns); i++ {
-		ns[i] = make([]comp, 0, 0)
+		ns[i] = make([]int,0,0)
 	}
 
 	for i := 0; i < M; i++ {
-		var f, s int
-		_, err = fmt.Fscan(rd, &f, &s) //   command
+		var f,s int
+		_, err = fmt.Fscan(rd, &f,&s) //   command
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
-		ns[f] = append(ns[f], comp{s, 0})
-		if f != s {
-			ns[s] = append(ns[s], comp{f, 0})
+		ns[f] =  append(ns[f], s )
+		if( f!=s){
+			ns[s] =  append(ns[s], f )
 		}
 	}
-	//fmt.Println( ns)
-	vis := make([]bool, N+1, N+1)
-comp:=1
-	for  i:=1 ; i<len(ns) {	
-		dfs(ns,vis,1, comp)
+ //fmt.Println( ns)
+ vis := make([] int, N+1,N+1) 	
+ for i := 1; i <len(vis) ; i++ {
+	vis[i] =-1
+ }
+comp := 0
+for i := 1; i < len(ns); i++ {
+	if vis[i]==-1 {
+		dfs(ns,vis,i,comp)
 		comp++
 	}
-	//dfs(ns, vis, 1)
-
+	
+} 
+/* fmt.Println( ns)
+fmt.Println(vis)
+ */
+ var ret string
+fmt.Println(comp)
+for i := 0; i < comp; i++ {
 	var ff string
-	count := 0
-	for i := 1; i < len(vis); i++ {
-
-		if vis[i] {
-			ff += fmt.Sprintf("%d ", i)
-			count++
-		}
+	var count int =0
+	for j:=1; j<len(vis);j++ {
+		if (vis[j]==i){
+		count++
+		ff += fmt.Sprint(j," ")	
+		}		
 	}
-	fmt.Println(count)
-	fmt.Println(ff)
+	ret += fmt.Sprint(count,"\n",ff,"\n")
+}
+fmt.Println(ret)
+/* dfs(ns,vis,1)
+
+ var ff string
+ count:=0
+for i := 1; i < len(vis); i++ {
+
+	if(vis[i]){
+		ff += fmt.Sprintf("%d ",i)
+		count++
+	}	
+} 	
+fmt.Println(count)
+fmt.Println(ff)*/
 }
